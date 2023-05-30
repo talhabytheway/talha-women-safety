8b7a8dd4ff78067477535861628378afa97e5762
Muhammad Talha <talhahanfsd@gmail.com>
2023-05-30 21:58:50 +0500

    Initialize React Components

---
+++ src/screens/SafetyTips.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const SafetyTips = () => {
+  return (
+    <View>
+      <Text>SafetyTips</Text>
+    </View>
+  );
+};
+
+export default SafetyTips;
+
+const styles = StyleSheet.create({});
